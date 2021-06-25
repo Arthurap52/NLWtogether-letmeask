92b40eb00b3128b1c085344402bce336d176ea20
Arthurap52 <josearthurantunespereira@gmail.com>
2021-06-25 01:14:09 -0300

    add arquivos aula4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import {createContext, useState, useEffect} from 'react'
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from './services/firebase';
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
-import { Room } from './pages/Room';
 
 function App() {
   return (
@@ -16,6 +15,8 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/rooms/new" exact component={NewRoom}/>
           <Route path="/rooms/:id" component={Room}/>
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}/>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
