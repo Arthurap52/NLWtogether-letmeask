e03bf805b3aae687ce37b89ec757a9bb0f9abe96
Arthurap52 <josearthurantunespereira@gmail.com>
2021-08-15 17:22:44 -0300

    update readme

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -30,7 +30,7 @@ export function Room() {
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
 
-    if (newQuestion.trim() == '') {
+    if (newQuestion.trim() === '') {
       return;
     }
 
